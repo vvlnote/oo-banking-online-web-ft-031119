bdfc472cc6aa3904e5a10f1add5e66d48dec320e
Joy Lin <vvlnote@hotmail.com>
2019-03-27 15:33:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid? ? true : false
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 end
